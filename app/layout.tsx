085bfb67908a5ab32f2be96312b81c4ea5764c44
Bernard Serino <cbmserino@gmail.com>
2026-03-31 09:41:09 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "app/globals.css";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Swake Diving Center | Freediving in Siquijor",
